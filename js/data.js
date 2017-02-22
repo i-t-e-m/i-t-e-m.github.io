b124d08c16b532799049f38b68449e13cbeb1330
Yano <ninian138@gmail.com>
2017-02-22 23:47:20 +0900

    githubから更新日時取得&Repository  class追加

--- js/data.js
+++ js/data.js
@@ -14,50 +14,43 @@ var DATA = {
 				+"<li>見やすいwebページにしたい</li>"
 				+"<li>Markdownファイルを扱いたい</li></ul>"
 				+"<p>という要望が出るであろう予測に応えて、その道しるべとなるように作られたマニュアルです</p>",
-			author: "minakawa",
-			modified: "2017/02/09"
+			author: "minakawa"
 		},
 		{
 			repository: "scala_newbie",
 			title: "Scala勉強会",
 			content: "Scalaとは2003年にできた比較的新しい言語です。 SclaはJVM（Java Virtual Machine）上で動作します。また、オブジェクト指向と関数型のハイブリット言語です。",
-			author: "sato",
-			modified: "2017/02/10"
+			author: "sato"
 		},
 		{
 			repository: "markdown_manual",
 			title: "Markdownマニュアル",
 			content: "今後IT学生が簡易な文章を見やすく書く必要性に駆られるだろうことを予想し、素早く綺麗に書くことができるMarkdownの基本書式を示したマニュアルです。",
-			author: "sato",
-			modified: "2017/02/19"
+			author: "sato"
 		},
 		{
 			repository: "nin-slider.js",
 			title: "nin-slider.js",
 			content: "HEWで使ったjsです。バグや要望があればITIまで。スライダーやタブを簡単に実装できます。",
-			author: "yano",
-			modified: "2017/02/14"
+			author: "yano"
 		},
 		{
 			repository: "clang_newbie",
 			title: "初めてのプログラム 〜C言語〜",
 			content: "C言語は古くから存在する言語です。そして、未だに消えない言語です。これからも消えなことでしょう。つまり、勉強して損をしない言語ということです。",
-			author: "sato",
-			modified: "2017/02/15"
+			author: "sato"
 		},
     {
 			repository: "sass-sample",
 			title: "初めてのスタイルシート言語 ~Sass~",
 			content: "Sass（サース)はハンプトン・キャトリンが設計しネイサン・バイゼンバウムが開発したスタイルシート言語です。これでフロントの開発速度向上になれば幸いです。",
-			author: "minakawa",
-			modified: "2017/02/19"
+			author: "minakawa"
 		},
 		{
 			repository: "introduction_of_Rust",
 			title: "Rust入門",
-			content: "RustとはMozillaによって開発されている並列かつマルチパラダイムの安全性、速度、並行性の3つにフォーカスしたシステムプログラミング言語です。",
-			author: "yano",
-			modified: "2017/02/19"
+			content: "RustはMozillaによって開発されている、高速に動作し、安全性、並行性、実用性にフォーカスしたシステムプログラミング言語です。",
+			author: "yano"
 		},
 	]
 };
@@ -65,6 +58,5 @@ var DATA = {
 		// 	repository: "",
 		// 	title: "",
 		// 	content: "",
-		// 	author: "",
-		// 	modified: ""
+		// 	author: ""
 		// },
--- js/repository.js
+++ js/repository.js
@@ -1,7 +1,37 @@
 "use strict"
+var Util = {
+	dateformat: function(date)  {
+		return date.getFullYear()+"/"+
+				("0"+( date.getMonth()+1 )).slice(-2)+"/"+
+				("0"+date.getDate()).slice(-2)+" "+
+				("0"+date.getHours()).slice(-2)+":"+
+				("0"+date.getMinutes()).slice(-2)+":"+
+				("0"+date.getSeconds()).slice(-2);
+	},
+	ajax: function(data, url, method, success, err) {
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			var result = document.getElementById('result');
+			if(req.readyState == 4) {
+				if (req.status >= 200 && req.status < 300) {
+					if(success) {
+						success(req.responseText);
+					}
+				} else {
+					if(err) err(req.responseText);
+				}
+			}
+		}
+		req.open(method, url, true);
+		req.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		req.send(data);
+	}
+};
+
+
 var repository;
 window.addEventListener("load", function() {
-	class Repository {
+	class RepositoryController {
 		constructor() {
 			this.el = document.getElementById("repositories");
 			this.repos = DATA.repository;
@@ -10,7 +40,7 @@ window.addEventListener("load", function() {
 		}
 		init() {
 			for(var i = this.repos.length - 1; 0 <= i; i--) {
-				this.repos[i].el = this.createRepo(this.repos[i]);
+				this.repos[i] = new Repository(this, this.repos[i]);
 				this.repos[i].title = this.toHalfSize(this.repos[i].title).toLowerCase();
 			}
 			this.inpSearch.addEventListener("keyup", this.search.bind(this));
@@ -35,30 +65,45 @@ window.addEventListener("load", function() {
     					.replace(/　/g, " ")
     					.replace(/〜/g, "~");
 		}
-		createRepo(repo) {
+	}
+
+
+
+	class Repository {
+		constructor(parent, data) {
+			this.parent = parent;
+			this.repository = data.repository;
+			this.title = data.title;
+			this.content = data.content;
+			this.author = data.author;
+			this.update;
+			this.el;
+			this.els = {};
+			this.createView();
+		}
+		createView() {
 			var wrap = document.createElement("li");
 			wrap.className = "repository";
 			var title = document.createElement("h2");
-			title.innerHTML = repo.title;
+			title.innerHTML = this.title;
 			title.className = "repo_title";
 			var dataWrap = document.createElement("div");
 			dataWrap.className = "repo_data-wrap";
 			var author = document.createElement("small");
 			author.className = "repo_author";
-			author.innerHTML = repo.author;
+			author.innerHTML = this.author;
 			var update = document.createElement("small");
 			update.className = "repo_update";
-			update.innerHTML = repo.modified;
 			var page_btn = document.createElement("a");
 			page_btn.className = "page-btn";
-			page_btn.setAttribute("href", "https://" + DATA.author[repo.author] + ".github.io/" + repo.repository);
+			page_btn.setAttribute("href", "https://" + DATA.author[this.author] + ".github.io/" + this.repository);
 			page_btn.innerHTML = "<span class=\"page-icon\"></span><span>page</span>";
 			var repo_btn = document.createElement("a");
 			repo_btn.className = "repo-btn";
-			repo_btn.setAttribute("href", "https://github.com/" + DATA.author[repo.author] + "/" + repo.repository);
+			repo_btn.setAttribute("href", "https://github.com/" + DATA.author[this.author] + "/" + this.repository);
 			repo_btn.innerHTML = "<span class=\"git-icon\"></span><span>repo</span>";
 			var content = document.createElement("div");
-			content.innerHTML = repo.content;
+			content.innerHTML = this.content;
 			wrap.appendChild(title);
 			wrap.appendChild(dataWrap);
 			dataWrap.appendChild(author);
@@ -66,9 +111,22 @@ window.addEventListener("load", function() {
 			wrap.appendChild(page_btn);
 			wrap.appendChild(repo_btn);
 			wrap.appendChild(content);
-			this.el.appendChild(wrap);
-			return wrap;
+			this.parent.el.appendChild(wrap);
+			this.els.update = update;
+			this.el = wrap;
+			this.getRepositoryData();
+		}
+		setUpdate(date) {
+			this.update = date;
+			this.els.update.innerHTML = Util.dateformat(date);
+		}
+		getRepositoryData() {
+			var url = "https://api.github.com/repos/" + DATA.author[this.author] + "/" + this.repository;
+			var success = function(data) {
+				this.setUpdate(new Date(JSON.parse(data).pushed_at));
+			}
+			Util.ajax("", url, "get", success.bind(this));
 		}
 	}
-	repository = new Repository();
+	repository = new RepositoryController();
 });
